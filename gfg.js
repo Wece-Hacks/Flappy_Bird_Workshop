34fa6ce494924353e668a43550a0400a32c475c7
Laura <122973125+lauragonzalezmoran@users.noreply.github.com>
2025-02-22 12:52:06 -0600

    Update gfg.js

--- gfg.js
+++ gfg.js
@@ -109,7 +109,70 @@ function play() {
 
 /////////////////////////////// Environment //////////////////////////////////
 
- 
+ let bird_dy = 0;
+  function apply_gravity() {
+    if (game_state != 'Play') return;
+    bird_dy = bird_dy + gravity;
+    document.addEventListener('keydown', (e) => {
+      if (e.key == 'ArrowUp' || e.key == ' ') {
+        bird_dy = -7.6;
+      }
+    });
+
+    // Collision detection with bird and
+    // window top and bottom
+
+    if (bird_props.top <= 0 ||
+        bird_props.bottom >= background.bottom) {
+      game_state = 'End';
+      message.innerHTML = 'Press Enter To Restart';
+      message.style.left = '28vw';
+      return;
+    }
+    bird.style.top = bird_props.top + bird_dy + 'px';
+    bird_props = bird.getBoundingClientRect();
+    requestAnimationFrame(apply_gravity);
+  }
+  requestAnimationFrame(apply_gravity);
+
+  let pipe_seperation = 0;
+  
+  // Constant value for the gap between two pipes
+  let pipe_gap = 35;
+  function create_pipe() {
+    if (game_state != 'Play') return;
+    
+    // Create another set of pipes
+    // if distance between two pipe has exceeded
+    // a predefined value
+    if (pipe_seperation > 115) {
+      pipe_seperation = 0
+      
+      // Calculate random position of pipes on y axis
+      let pipe_posi = Math.floor(Math.random() * 43) + 8;
+      let pipe_sprite_inv = document.createElement('div');
+      pipe_sprite_inv.className = 'pipe_sprite';
+      pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
+      pipe_sprite_inv.style.left = '100vw';
+      
+      // Append the created pipe element in DOM
+      document.body.appendChild(pipe_sprite_inv);
+      let pipe_sprite = document.createElement('div');
+      pipe_sprite.className = 'pipe_sprite';
+      pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
+      pipe_sprite.style.left = '100vw';
+      pipe_sprite.increase_score = '1';
+      
+      // Append the created pipe element in DOM
+      document.body.appendChild(pipe_sprite);
+    }
+    pipe_seperation++;
+     //////////////////// FINISH THE CODE: UNCOMMENT BELOW AND FILL IN /////////////////
+    // fill out this line so that we continue the recursive loop by scheduling the next pipe creation
+  }
+  requestAnimationFrame(create_pipe);
+}
+///////////
 
 
 
