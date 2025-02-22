176aaeb6561d1c46e90479f870793038d399ff40
Laura <122973125+lauragonzalezmoran@users.noreply.github.com>
2025-02-22 12:34:53 -0600

    Update gfg.js

--- gfg.js
+++ gfg.js
@@ -28,8 +28,25 @@ let background =
 
 /////////////////////////// Start ////////////////////////////////////////
   
-
-
+// Add an eventlistener for key presses
+document.addEventListener('keydown', (e) => {
+  
+  // Start the game if enter key is pressed
+  if (e.key == 'Enter' &&
+      game_state != 'Play') {
+    document.querySelectorAll('.pipe_sprite')
+              .forEach((e) => {
+      e.remove();
+    });
+    bird.style.top = '40vh';
+    ///////////// FINISH THE CODE: UNCOMMENT BELOW AND FILL IN //////////////////
+    // game_state =       
+    // message.innerHTML = 
+    score_title.innerHTML = 'Score : ';
+    // score_val.innerHTML = 
+    // fill this line to call the play function
+  }
+});
 
 
 //////////////////////////// Move ////////////////////////////////////////
